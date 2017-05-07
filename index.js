f933221884802c1a2a205dc1014fc802c59b32fc
Erik Krietsch <erik.krietsch@gmail.com>
2017-05-07 15:32:58 -0500

    Add getPlayer method

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ exports.listPlayers = function (event, context, callback) {
 	})
 	.catch(err => error.handle(err, callback))
 };
+
+exports.getPlayer = function (event, context, callback) {
+	matchmaker.getPlayer(event)
+	.then(response => {
+		callback(null, response)
+	})
+	.catch(err => error.handle(err, callback))
+}
